c15b3eb05ff79cc1c2fce3ae0d49cf8defa89897
jw0293 <k38836919@gmail.com>
2022-08-12 15:04:13 +0900

    프로젝트 생성

---
+++ src/main/java/com/spring_chatting/SpringChattingApplication.java
@@ -0,0 +1,13 @@
+package com.spring_chatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChattingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChattingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_chatting/SpringChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_chatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChattingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
